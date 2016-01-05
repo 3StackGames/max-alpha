f3517f978ada139cdb39f36a732936eda006ea6c
Jason Lin <j@sonl.in>
2016-01-05 03:20:22 -0600

    .

--- src/main/java/com/three_stack/maximum_alpha/backend/game/Parameters.java
+++ src/main/java/com/three_stack/maximum_alpha/backend/game/Parameters.java
@@ -7,7 +7,7 @@ import com.three_stack.maximum_alpha.backend.server.Connection;
 public class Parameters {
     final int INITIAL_DRAW_SIZE = 7;
     final int TOTAL_HEALTH = 40;
-    final int INITIAL_COLORLESS_MANA = 1;
+    public final static int INITIAL_COLORLESS_MANA = 1;
 
     public List<Connection> players;
 
