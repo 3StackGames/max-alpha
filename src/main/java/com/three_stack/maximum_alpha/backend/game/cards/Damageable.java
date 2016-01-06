747269ec84037a164a601f64ac77c6c6edc0f7f2
Richard Shen <richarshen@gmail.com>
2016-01-05 23:22:47 -0600

    my merge

--- src/main/java/com/three_stack/maximum_alpha/backend/game/cards/Damageable.java
+++
@@ -1,7 +0,0 @@
-package com.three_stack.maximum_alpha.backend.game.cards;
-
-import com.three_stack.maximum_alpha.backend.game.events.Event;
-
-public interface Damageable {
-    Event takeDamage(int damage, Card source);
-}
