f830f3471bd33850f8df9d293e212edf8285879b
Richard Shen <richarshen@gmail.com>
2016-01-08 00:00:27 -0600

    forgot to update a prompt class

--- src/main/java/com/three_stack/maximum_alpha/backend/game/prompts/ChoosePrompt.java
+++ src/main/java/com/three_stack/maximum_alpha/backend/game/prompts/ChoosePrompt.java
@@ -3,12 +3,13 @@ package com.three_stack.maximum_alpha.backend.game.prompts;
 import java.util.List;
 
 import com.three_stack.maximum_alpha.backend.game.cards.Card;
+import com.three_stack.maximum_alpha.backend.game.player.Player;
 
 //@Todo: Do this
 public abstract class ChoosePrompt extends Prompt {
 
-    public ChoosePrompt(Card source, List<Card> options) {
-        super(source);
+    public ChoosePrompt(Card source, Player player, List<Card> options) {
+        super(source, player);
         this.options = options;
     }
 
