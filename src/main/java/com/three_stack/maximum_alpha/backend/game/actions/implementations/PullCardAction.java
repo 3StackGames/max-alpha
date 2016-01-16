84827e5e43b5442474706ec002bca688df925bfc
Jason Lin <jlin51@illinois.edu>
2016-01-15 23:48:19 -0800

    implemented ON_PULL

--- src/main/java/com/three_stack/maximum_alpha/backend/game/actions/implementations/PullCardAction.java
+++ src/main/java/com/three_stack/maximum_alpha/backend/game/actions/implementations/PullCardAction.java
@@ -1,5 +1,7 @@
 package com.three_stack.maximum_alpha.backend.game.actions.implementations;
 
+import com.three_stack.maximum_alpha.backend.game.events.SingleCardEvent;
+import com.three_stack.maximum_alpha.backend.game.events.Trigger;
 import com.three_stack.maximum_alpha.backend.game.player.Player;
 import com.three_stack.maximum_alpha.backend.game.State;
 import com.three_stack.maximum_alpha.backend.game.actions.abstracts.ExistingCardAction;
@@ -15,20 +17,21 @@ public class PullCardAction extends ExistingCardAction {
 
         player.getHand().add(pullCard, state);
         player.setHasAssignedOrPulled(true);
-        
-        Event event = new Event(player.getUsername() + " has pulled " + pullCard.getName() + " from work");
+
+        Event event = new SingleCardEvent(player, player.getUsername() + " has pulled " + pullCard.getName() + " from work", pullCard);
         state.addEvent(event);
+        state.notify(Trigger.ON_PULL, event);
     }
 
-	@Override
-	public boolean isValid(State state) {
-		boolean notInPrompt = notInPrompt(state);
-		boolean correctPhase = isPhase(state, "Main Phase"); //TODO: FubarPhase (instant phase)
-		boolean playerTurn = isPlayerTurn(state);
-		boolean playerCanPull = player.canAssignOrPull();
-		Creature creature = player.getTown().findCard(cardId);
-		boolean isPullable = creature.isPullable();
-		
-		return notInPrompt && correctPhase && playerTurn && playerCanPull && isPullable;
-	}
+    @Override
+    public boolean isValid(State state) {
+        boolean notInPrompt = notInPrompt(state);
+        boolean correctPhase = isPhase(state, "Main Phase"); //TODO: FubarPhase (instant phase)
+        boolean playerTurn = isPlayerTurn(state);
+        boolean playerCanPull = player.canAssignOrPull();
+        Creature creature = player.getTown().findCard(cardId);
+        boolean isPullable = creature.isPullable();
+
+        return notInPrompt && correctPhase && playerTurn && playerCanPull && isPullable;
+    }
 }
