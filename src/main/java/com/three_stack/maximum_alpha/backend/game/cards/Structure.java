bb23d7581d8735bf12559315b44dd27079797367
Jason Lin <jlin51@illinois.edu>
2016-01-19 09:51:01 -0600

    added attackable flag

--- src/main/java/com/three_stack/maximum_alpha/backend/game/cards/Structure.java
+++ src/main/java/com/three_stack/maximum_alpha/backend/game/cards/Structure.java
@@ -9,10 +9,12 @@ import java.util.Map;
 
 public class Structure extends DamageableCard {
     protected boolean underConstruction;
+    protected boolean attackable;
 
     public Structure(String name, ResourceList cost, String text, String flavorText, int health, Map<Trigger, List<Effect>> effects) {
         super(name, cost, text, flavorText, health, effects);
         underConstruction = true;
+        attackable = true;
     }
 
     public Structure(Structure other) {
@@ -26,5 +28,13 @@ public class Structure extends DamageableCard {
 
     public void setUnderConstruction(boolean underConstruction) {
         this.underConstruction = underConstruction;
+    }
+
+    public boolean isAttackable() {
+        return attackable;
+    }
+
+    public void setAttackable(boolean attackable) {
+        this.attackable = attackable;
     }
 }
