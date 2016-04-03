67a5e98cc0e148758c8092a47eac00b076569060
Richard Shen <richarshen@gmail.com>
2016-04-03 16:16:08 -0500

    Spawn creatures now targets player(s)

--- src/main/java/com/three_stack/maximum_alpha/backend/game/effects/results/implementations/SpawnResult.java
+++ src/main/java/com/three_stack/maximum_alpha/backend/game/effects/results/implementations/SpawnResult.java
@@ -1,25 +1,29 @@
 package com.three_stack.maximum_alpha.backend.game.effects.results.implementations;
 
-import com.three_stack.maximum_alpha.backend.game.State;
-import com.three_stack.maximum_alpha.backend.game.Time;
-import com.three_stack.maximum_alpha.backend.game.cards.Card;
-import com.three_stack.maximum_alpha.backend.game.cards.CardFactory;
-import com.three_stack.maximum_alpha.backend.game.cards.Creature;
-import com.three_stack.maximum_alpha.backend.game.cards.Structure;
-import com.three_stack.maximum_alpha.backend.game.effects.events.Event;
-import com.three_stack.maximum_alpha.backend.game.effects.results.Result;
-import com.three_stack.maximum_alpha.backend.game.player.Player;
-import com.three_stack.maximum_alpha.backend.game.utilities.DatabaseClientFactory;
-import com.three_stack.maximum_alpha.database_client.pojos.DBCard;
-import com.three_stack.maximum_alpha.database_client.pojos.DBResult;
-import org.bson.types.ObjectId;
-
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class SpawnResult extends Result{
+import org.bson.types.ObjectId;
+
+import com.three_stack.maximum_alpha.backend.game.State;
+import com.three_stack.maximum_alpha.backend.game.Time;
+import com.three_stack.maximum_alpha.backend.game.cards.Card;
+import com.three_stack.maximum_alpha.backend.game.cards.CardFactory;
+import com.three_stack.maximum_alpha.backend.game.cards.Creature;
+import com.three_stack.maximum_alpha.backend.game.cards.NonSpellCard;
+import com.three_stack.maximum_alpha.backend.game.cards.Structure;
+import com.three_stack.maximum_alpha.backend.game.effects.events.Event;
+import com.three_stack.maximum_alpha.backend.game.effects.results.Result;
+import com.three_stack.maximum_alpha.backend.game.effects.results.TargetResult;
+import com.three_stack.maximum_alpha.backend.game.effects.results.TargetStep;
+import com.three_stack.maximum_alpha.backend.game.player.Player;
+import com.three_stack.maximum_alpha.backend.game.utilities.DatabaseClientFactory;
+import com.three_stack.maximum_alpha.database_client.pojos.DBCard;
+import com.three_stack.maximum_alpha.database_client.pojos.DBResult;
+
+public class SpawnResult extends TargetResult {
     //@Todo: specify owner
     //@Todo: Add deck
     //@Todo: @Jason move values to value object for consistency
@@ -30,6 +34,13 @@ public class SpawnResult extends Result{
     protected DBCard cardTemplate;
     protected int count;
     protected SpawnZone spawnZone;
+    
+    public SpawnResult(List<TargetStep> targetSteps, DBCard cardTemplate, int count, SpawnZone spawnZone) {
+        super(targetSteps);
+        this.cardTemplate = cardTemplate;
+        this.count = count;
+        this.spawnZone = spawnZone;
+    }
 
     public SpawnResult(DBResult dbResult) {
         super(dbResult);
@@ -59,30 +70,34 @@ public class SpawnResult extends Result{
 
     @Override
     public void resolve(State state, Card source, Event event, Map<String, Object> value) {
-        Player player = source.getController();
-        List<Card> cards = IntStream.range(0, count)
-                .mapToObj(index -> CardFactory.create(cardTemplate))
-                .collect(Collectors.toList());
-        state.trackCardEffectsAndMarkController(cards, player);
-        Time spawnTime = state.getTime();
-        switch (spawnZone) {
-            case FIELD:
-                List<Creature> creatures = cards.stream()
-                        .map(card -> (Creature) card)
-                        .collect(Collectors.toList());
-                player.getField().addAll(creatures, spawnTime, state);
-                break;
-            case COURTYARD:
-                List<Structure> structures = cards.stream()
-                        .map(card -> (Structure) card)
-                        .collect(Collectors.toList());
-                player.getCourtyard().addAll(structures, spawnTime, state);
-                break;
-            case HAND:
-                player.getHand().addAll(cards, spawnTime, state);
-                break;
-            default:
-                throw new IllegalStateException("spawn zone not recognized");
+        List<NonSpellCard> targets = (List<NonSpellCard>) value.get("targets");
+        List<Player> players = targets.stream().map(NonSpellCard::getController).collect(Collectors.toList());
+
+        for(Player player : players) {
+            List<Card> cards = IntStream.range(0, count)
+                    .mapToObj(index -> CardFactory.create(cardTemplate))
+                    .collect(Collectors.toList());
+	        state.trackCardEffectsAndMarkController(cards, player);
+	        Time spawnTime = state.getTime();
+	        switch (spawnZone) {
+	            case FIELD:
+	                List<Creature> creatures = cards.stream()
+	                        .map(card -> (Creature) card)
+	                        .collect(Collectors.toList());
+	                player.getField().addAll(creatures, spawnTime, state);
+	                break;
+	            case COURTYARD:
+	                List<Structure> structures = cards.stream()
+	                        .map(card -> (Structure) card)
+	                        .collect(Collectors.toList());
+	                player.getCourtyard().addAll(structures, spawnTime, state);
+	                break;
+	            case HAND:
+	                player.getHand().addAll(cards, spawnTime, state);
+	                break;
+	            default:
+	                throw new IllegalStateException("spawn zone not recognized");
+	        }
         }
     }
 
