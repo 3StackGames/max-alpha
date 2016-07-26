a9ec2e84b8d6c7d7269556e41207835a73c38471
Richard Shen <rshen@ixl.corp>
2016-07-26 15:32:36 -0700

    added casting

--- src/main/java/com/three_stack/maximum_alpha/backend/game/utilities/ValueExpression.java
+++ src/main/java/com/three_stack/maximum_alpha/backend/game/utilities/ValueExpression.java
@@ -5,50 +5,44 @@ import java.util.Map;
 
 import com.three_stack.maximum_alpha.backend.game.State;
 
+//TODO: move to better location
+//TODO: something else - forgot what
 public class ValueExpression {
 
-  private Expression expression;
-  private char variable = 'a';
-  private boolean constant;
-  private int amount;
+	private Object value;
 
-  public ValueExpression (Object value) {
-    if (value.getClass() == int.class) {
-      amount = (int) value;
-      constant = true;
-    } 
-    else {
-      expression = new Expression(value);
-      constant = false;
-    }
-  }
+	public ValueExpression (Object value) {
+		this.value = value;
+	}
 
-  public int eval(State state) {
-    if (!constant) {  
-      List<Map<String, Object>> variables = (List<Map<String, Object>>) value.get("variables"); //TODO fix
-  
-      for (Map<String, Object> variable : variables) {
-        String type = (String) variable.get("type");
-    
-        switch (type) {
-          case "attribute": 
-            String attributeName = (String) variable.get("attribute");
-            Attribute attribute = state.getAttributes().get(attributeName);         
-            List<String> filterNames = (List<String>) variable.get("filters");
-            List<Filter> filters = filterNames.stream().
-                map(filter -> Filter.valueOf(filter)).collect(Collectors.toList());
-            
-            expression.with(variable++, attribute.process(filters));
-            break;
-          default:
-            break;
-        }
-      }
-  
-      variable = 'a';
-      return expression.eval();
-    }
-    
-    return amount;
-  }
+	public int eval(State state) {
+		if (value.getClass() == int.class) {
+			return (int) value;
+		}
+
+		Map<String, Object> expressionValue = (Map<String, Object>) value;
+		Expression expression = new Expression((String) expressionValue.get("expression"));
+		char variableName = 'a';
+		List<Map<String, Object>> variables = (List<Map<String, Object>>) expressionValue.get("variables");
+
+		for (Map<String, Object> variable : variables) {
+			String type = (String) variable.get("type");
+
+			switch (type) {
+				case "attribute": 
+					String attributeName = (String) variable.get("attribute");
+					Attribute attribute = state.getAttributes().get(attributeName);         
+					List<String> filterNames = (List<String>) variable.get("filters");
+					List<Filter> filters = filterNames.stream().
+						map(filter -> Filter.valueOf(filter)).collect(Collectors.toList());
+					
+					expression.with(variableName++, attribute.process(filters));
+					break;
+				default:
+					break;
+			}
+		}
+
+		return expression.eval();
+	}
 }
